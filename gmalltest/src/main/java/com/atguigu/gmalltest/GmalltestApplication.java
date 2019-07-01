4e0d0ecc13877ea77400a56cdaa1ca7dd55e2d63
sunxj <751256015@qq.com>
2019-07-01 13:42:45 +0800

    111111

---
+++ gmalltest/src/main/java/com/atguigu/gmalltest/GmalltestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmalltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmalltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmalltestApplication.class, args);
+	}
+
+}
---
+++ gmalltest/src/test/java/com/atguigu/gmalltest/GmalltestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmalltest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmalltestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
